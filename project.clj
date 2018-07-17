eb596f0547e90f22b25dd56f0f79a953096d1552
Wilker Lucio <wilkerlucio@gmail.com>
2018-07-17 10:35:41 -0300

    Workspaces from maven

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
   :plugins [[lein-tools-deps "0.4.1"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :lein-tools-deps/config {:config-files [:install :user :project]
-                           :aliases [:dev :workspaces-dev]}
+                           :aliases [:dev]}
   :jar-exclusions [#"public/.*" #"^workspaces/.*" #"\.DS_Store"]
   :profiles {:dev {:source-paths ["src" "workspaces"]}})
