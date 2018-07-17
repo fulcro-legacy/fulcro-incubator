f81ed821e086f304b3f3090a1df77c1ed55ebd7d
Wilker Lucio <wilkerlucio@gmail.com>
2018-07-16 21:25:06 -0300

    Bring files and initial workspaces compilation

---
+++ src/deps.cljs
@@ -0,0 +1,2 @@
+{:npm-deps {:reakit      "^0.11.1"
+            :react-icons "^2.2.7"}}
---
+++ workspaces/fulcro/incubator/workspaces/main.cljs
@@ -0,0 +1,4 @@
+(ns fulcro.incubator.workspaces.main
+  (:require [nubank.workspaces.core :as ws]))
+
+(ws/mount)
