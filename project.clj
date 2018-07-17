e73541218fceafd2c42e396a1b46104b9441d40a
Wilker Lucio <wilkerlucio@gmail.com>
2018-07-17 15:06:07 -0300

    Fix description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject fulcro-incubator "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Tools for Fulcro apps"
+  :url "https://github.com/fulcrologic/fulcro-incubator"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-tools-deps "0.4.1"]]
