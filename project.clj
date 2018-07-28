045374c263458b6018b00a8a75b8bcca4d04af2e
Wilker Lucio <wilkerlucio@gmail.com>
2018-07-28 07:04:36 -0300

    Don't use dev alias on main lein config

--- project.clj
+++ project.clj
@@ -2,10 +2,9 @@
   :description "Tools for Fulcro apps"
   :url "https://github.com/fulcrologic/fulcro-incubator"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-tools-deps "0.4.1"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
-  :lein-tools-deps/config {:config-files [:install :user :project]
-                           :aliases [:dev]}
+  :lein-tools-deps/config {:config-files [:install :user :project]}
   :jar-exclusions [#"public/.*" #"^workspaces/.*" #"\.DS_Store"]
   :profiles {:dev {:source-paths ["src" "workspaces"]}})
