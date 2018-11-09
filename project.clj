83a6ca85270f384ea04139ae36ad433305c71677
Tony Kay <tony.kay@gmail.com>
2018-11-08 17:51:14 -0800

    updated version and changelog

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fulcrologic/fulcro-incubator "0.0.14-SNAPSHOT"
+(defproject fulcrologic/fulcro-incubator "0.0.14"
   :description "Tools for Fulcro apps"
   :url "https://github.com/fulcrologic/fulcro-incubator"
   :license {:name "MIT" :url "https://opensource.org/licenses/MIT"}
