bb834df98170d675e2783d7fd2d8ac14fb39717f
Tony Kay <tony.kay@gmail.com>
2019-07-23 15:03:31 -0700

    fixed path matching for maximal path

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fulcrologic/fulcro-incubator "0.0.37"
+(defproject fulcrologic/fulcro-incubator "0.0.38"
   :description "Tools for Fulcro apps"
   :url "https://github.com/fulcrologic/fulcro-incubator"
   :license {:name "MIT" :url "https://opensource.org/licenses/MIT"}
